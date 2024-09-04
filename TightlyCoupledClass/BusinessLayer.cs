0fd2f7bd6891fd24fdc511ba0f6414889ca89342
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2024-09-04 13:15:16 +0300

    Add project files.

---
+++ TightlyCoupledClass/BusinessLayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TightlyCoupledClass
+{
+    public class BusinessLayer
+    {
+        private DataAccessLayer _dataAccessLayer { get; set; }
+
+        public BusinessLayer()
+        {
+            _dataAccessLayer = new DataAccessLayer();
+        }
+
+        public List<Product> GetProducts()
+        {
+            return _dataAccessLayer.GetProducts();
+        }
+    }
+}
---
+++ TightlyCoupledClass/DataAccessLayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TightlyCoupledClass
+{
+    public class DataAccessLayer
+    {
+        public List<Product> GetProducts()
+        {
+            return new List<Product>()
+            {
+                new Product { Id = 1, Name= "Kalem", Price=100, Stock=50 },
+                new Product { Id = 2, Name= "Kitap", Price=400, Stock=150 },
+                new Product { Id = 3, Name= "Silgi", Price=200, Stock=250 },
+                new Product { Id = 4, Name= "Defter", Price=600, Stock=550 },
+            };
+        }
+    }
+}
