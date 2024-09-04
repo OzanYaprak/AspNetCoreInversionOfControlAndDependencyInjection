81df93984c353701ab4d509f976b64ea8d953183
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2024-09-04 13:20:26 +0300

    #

--- FactoryPatternIOCImplementation/Program.cs
+++ FactoryPatternIOCImplementation/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FactoryPatternIOCImplementation;
 
 namespace FactoryPatternIOCImplementation
 {
@@ -10,6 +11,15 @@ namespace FactoryPatternIOCImplementation
     {
         static void Main(string[] args)
         {
+            BusinessLayer businessLayer = new BusinessLayer();
+
+            var products = businessLayer.GetProducts();
+
+            foreach (var item in products)
+            {
+                Console.WriteLine($"ID:{item.Id} Ad:{item.Name} Fiyat:{item.Price} Stok:{item.Stock}");
+            }
+            Console.ReadLine();
         }
     }
 }
