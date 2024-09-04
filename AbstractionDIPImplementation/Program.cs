33b5e84bcdd0043bc8c0bec47c7a11c27e22ca70
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2024-09-04 13:31:36 +0300

    #

---
+++ AbstractionDIPImplementation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractionDIPImplementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessLayer businessLayer = new BusinessLayer();
+
+            var products = businessLayer.GetProducts();
+
+            foreach (var item in products)
+            {
+                Console.WriteLine($"ID:{item.Id} Ad:{item.Name} Fiyat:{item.Price} Stok:{item.Stock}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
